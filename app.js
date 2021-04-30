8dfe558d5537a78bdea8be7d0714c1efc849198b
imrulkayes-nits <emimrulkayes.nits@gmail.com>
2021-04-30 14:46:13 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// Different Type Of Data Types 
+
+/* No: 01. String 
+----------------------*/
+var x = "Hello world";
+
+/* No: 02. Number
+----------------------*/
+var x = 1234;
+
+/* No: 03. Boolean
+----------------------*/
+var x = true / false;
+// var x = 1; (true)
+// var x = 0; (false)
+
+/* No: 04. Array
+----------------------*/
+var x = ["HTML", "CSS", "Javascript", "Java"];
+
+/* No: 05. Object
+----------------------*/
+var x = {first:"john", last:"Doe"};
+
+/* No: 06. Null 
+----------------------*/
+var x = null;
+
+/* No: 07. Undefined 
+----------------------*/
+var x;
+
